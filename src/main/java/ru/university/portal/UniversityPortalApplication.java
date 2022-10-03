e2a956b60121aa293502e337eb1b7f42fa4cf61d
Polina Chulkova <shelkova1937@gmail.com>
2022-10-03 21:26:25 +0500

    Создание проекта

---
+++ src/main/java/ru/university/portal/UniversityPortalApplication.java
@@ -0,0 +1,13 @@
+package ru.university.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityPortalApplication.class, args);
+    }
+
+}
