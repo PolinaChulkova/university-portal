d1ce6cbc84ddbffb2aa56e5aa3c13ddbabaf51fa
Polina Chulkova <shelkova1937@gmail.com>
2022-10-06 16:48:09 +0500

    Были добавленны классы сервисов

---
+++ src/main/java/ru/university/portal/service/GroupService.java
@@ -0,0 +1,13 @@
+package ru.university.portal.service;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Slf4j
+public class GroupService {
+}
