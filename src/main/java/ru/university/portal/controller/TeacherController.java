696a70a9267dc693da0de8a7d924d4ecf2e3929e
Polina Chulkova <shelkova1937@gmail.com>
2022-10-24 14:50:38 +0500

    Добавлены CRUD опрерации в TeacherController

--- src/main/java/ru/university/portal/controller/TeacherController.java
+++ src/main/java/ru/university/portal/controller/TeacherController.java
@@ -3,8 +3,8 @@ package ru.university.portal.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.university.portal.dto.CreateRatingDTO;
 import ru.university.portal.dto.MessageResponse;
+import ru.university.portal.dto.TeacherDTO;
 import ru.university.portal.service.RatingService;
 import ru.university.portal.service.SubjectService;
 import ru.university.portal.service.TeacherService;
@@ -18,5 +18,36 @@ public class TeacherController {
     private final TeacherService teacherService;
     private final SubjectService subjectService;
 
+    @GetMapping("/groups/{teacherId}")
+    public ResponseEntity<?> findTeacherGroups(@PathVariable Long teacherId) {
+        return ResponseEntity.ok().body(teacherService.findTeacherGroups(teacherId));
+    }
 
+    @GetMapping("/{teacherId}")
+    public ResponseEntity<?> findTeacherById(@PathVariable Long teacherId) {
+        return ResponseEntity.ok().body(teacherService.findTeacherById(teacherId));
+    }
+
+    //    для админа
+    @PostMapping("/create")
+    public ResponseEntity<?> findTeacherGroups(@RequestBody TeacherDTO dto) {
+        teacherService.createTeacher(dto);
+        return ResponseEntity.ok().body(new MessageResponse("Создан преподаватель с email: " + dto.getEmail()));
+    }
+
+    //    для админа
+    @PutMapping("/update/{teacherId}")
+    public ResponseEntity<?> findTeacherGroups(@PathVariable Long teacherId,
+                                               @RequestBody TeacherDTO dto) {
+        teacherService.updateTeacher(teacherId, dto);
+        return ResponseEntity.ok().body(new MessageResponse("Обновлён преподаватель с email: "
+                + dto.getEmail()));
+    }
+
+    //    для админа
+    @DeleteMapping("/{teacherId}")
+    public ResponseEntity<?> deleteTeacherById(@PathVariable Long teacherId) {
+        teacherService.deleteTeacherById(teacherId);
+        return ResponseEntity.ok().body(new MessageResponse("Удалён преподаватель с id=" + teacherId));
+    }
 }
